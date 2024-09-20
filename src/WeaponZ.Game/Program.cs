68806c6b61fab05bd4851623f7506f12f6093cab
Bruno Camargo <bruno.br.camargo@gmail.com>
2024-09-19 23:14:49 -0300

    boilerplate

--- src/WeaponZ.Game/Program.cs
+++ src/WeaponZ.Game/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
+
+namespace WeaponZ.Game;
+
+public class Program
+{
+    private static GraphicsDevice? _graphicsDevice;
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, WeaponZ!");
+
+        WindowCreateInfo windowCI = new WindowCreateInfo()
+        {
+            X = 100,
+            Y = 100,
+            WindowWidth = 800,
+            WindowHeight = 600,
+            WindowTitle = "WeaponZ"
+        };
+
+        Sdl2Window window = VeldridStartup.CreateWindow(ref windowCI);
+
+        GraphicsDeviceOptions options = new()
+        {
+            PreferStandardClipSpaceYDirection = true,
+            PreferDepthRangeZeroToOne = true
+        };
+
+        _graphicsDevice = VeldridStartup.CreateGraphicsDevice(window, options);
+
+        while (window.Exists)
+        {
+            window.PumpEvents();
+        }
+    }
+}
